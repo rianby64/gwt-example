be032f0edb8746db6b7166490d67e4273ce7f346
Andres Rios <rianby64@gmail.com>
2020-12-23 02:48:40 +0300

    Improve run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
 podman build -f Containerfile.simple -t gwt-myapp .
-podman run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri --net=host --security-opt=label=type:container_runtime_t -e DISPLAY gwt-myapp
+
+podman run --rm -it \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /dev/dri:/dev/dri \
+  -p 8888:8888 \
+  -p 9876:9876 \
+  --security-opt=label=type:container_runtime_t \
+  -e DISPLAY \
+  gwt-myapp
